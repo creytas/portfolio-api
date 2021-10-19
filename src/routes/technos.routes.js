aedf086e0826ae26d9ab27adccb93593f25d31c3
CR3Y745 <creytas@gmail.com>
2021-10-19 12:18:29 +0100

    feat: define techno routes

--- src/routes/technos.routes.js
+++ src/routes/technos.routes.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const technoRoutes = express.Router();
+
+technoRoutes.get("/all");
+technoRoutes.get("/:id");
+technoRoutes.post("/new-techno");
+technoRoutes.put("/update-techno/:id");
+technoRoutes.delete("/delete-techno/:id");
+technoRoutes.delete("/delete-techno/all");
+
+module.exports = technoRoutes;
