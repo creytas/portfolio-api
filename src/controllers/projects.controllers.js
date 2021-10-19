79867c4d221bb0fe343c8fa43383b8cf7398e949
CR3Y745 <creytas@gmail.com>
2021-10-19 14:14:46 +0100

    feat: define project controllers

--- src/controllers/projects.controllers.js
+++ src/controllers/projects.controllers.js
@@ -1,3 +1,35 @@
 const getAllProject = async (req, res) => {
   res.send({ message: "voici tous les projets" });
 };
+const getProjectById = async (req, res) => {
+  res.send({ message: "voici le projet " + req.params.id + " ." });
+};
+const newProject = async (req, res) => {
+  res.send({
+    message: "le projet " + req.params.id + " a ete creee avec succes",
+  });
+};
+const updateProject = async (req, res) => {
+  res.send({
+    message:
+      "la mise a jour du projet " + req.params.id + " a ete faite avec succes",
+  });
+};
+const deleteProjectById = async (req, res) => {
+  res.send({
+    message:
+      "la suppression du projet " + req.params.id + " a ete faite avec succes",
+  });
+};
+const deleteAllProject = async (req, res) => {
+  res.send({ message: "tous les projets ont ete supprimes" });
+};
+
+module.exports = {
+  getAllProject,
+  getProjectById,
+  newProject,
+  updateProject,
+  deleteProjectById,
+  deleteAllProject,
+};
