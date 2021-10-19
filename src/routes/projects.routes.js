567f9e3c115092ee652cf93e9db3a13437b76b70
CR3Y745 <creytas@gmail.com>
2021-10-19 12:06:33 +0100

    feat: define project routes

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const projectRoutes = express.Router();
+
+projectRoutes.get("/all");
+projectRoutes.get("/:id");
+projectRoutes.post("/new-project");
+projectRoutes.put("/update-project/:id");
+projectRoutes.delete("/delete-project/:id");
+projectRoutes.delete("/delete-project/all");
+
+module.exports = projectRoutes;
