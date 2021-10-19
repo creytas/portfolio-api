99e03c54453953755926fd4c9b21ffe27fc11a10
CR3Y745 <creytas@gmail.com>
2021-10-19 12:28:33 +0100

    feat: define user routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,2 +1,11 @@
 const express = require("express");
 const userRoutes = express.Router();
+
+userRoutes.get("/all");
+userRoutes.get("/:id");
+userRoutes.post("/new-user");
+userRoutes.put("/update-user/:id");
+userRoutes.delete("/delete-user/:id");
+userRoutes.delete("/delete-user/all");
+
+module.exports = userRoutes;
